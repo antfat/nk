797bb5a0b5e2cdf39c3aad8a4934ad49d3df26c3
antfat <32416518+antfat@users.noreply.github.com>
2025-09-27 13:21:32 +0300

    Add files via upload

---
+++ run_t.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# ==== НАСТРОЙКИ ====
+WORKDIR="$HOME/work"
+WORKER="$WORKDIR/worker"
+MINER_URL="https://github.com/SWPSCO/nockpool-miner-launcher/releases/download/v0.2.2/miner-launcher_linux_x64"
+
+# Константы для запуска майнера
+LABEL="workers"
+TOKEN="nockacct_47687ac45a58d0e49e1bc72ea24d7552"
+SUFFIX="$1"   # изменяемая часть имени (01, 02, 03...)
+
+if [ -z "$SUFFIX" ]; then
+  exit 1
+fi
+
+# ==== ОБНОВЛЕНИЕ ПАПКИ ====
+if [ -d "$WORKDIR" ]; then
+  rm -rf "$WORKDIR"
+fi
+mkdir -p "$WORKDIR"
+
+# ==== СКАЧИВАНИЕ МАЙНЕРА ====
+wget -O "$WORKER" "$MINER_URL"
+chmod +x "$WORKER"
+
+cp worker /usr/local/bin/miner-launcher
+
+# ==== ОПРЕДЕЛЕНИЕ РЕСУРСОВ ====
+CPU_CORES=$(nproc)
+GPU_COUNT=$(nvidia-smi -L 2>/dev/null | wc -l)
+
+if [ "$GPU_COUNT" -eq 0 ]; then
+  echo "❌ GPU не обнаружены!"
+  exit 1
+fi
+
+REQUIRED_THREADS=$((GPU_COUNT * 24))
+if [ "$CPU_CORES" -ge "$REQUIRED_THREADS" ]; then
+  THREADS_PER_CARD=24
+else
+  THREADS_PER_CARD=$((CPU_CORES / GPU_COUNT))
+  TOTAL_THREADS=(THREADS_PER_CARD * GPU_COUNT)
+fi
+
+# ==== ФОРМИРОВАНИЕ ИМЕНИ ====
+WORKER_NAME="r$SUFFIX"
+
+# ==== ЗАПУСК С ЛОГАМИ В КОНСОЛЬ ====
+while true; do
+  "$WORKER" \
+    --max-threads="$TOTAL_THREADS" \
+    --label="$LABEL" \
+    --account-token="$TOKEN"
+
+  EXIT_CODE=$?
+  echo "❌ Майнер остановился (код $EXIT_CODE). Перезапуск через 5 секунд..."
+  sleep 5
+done
